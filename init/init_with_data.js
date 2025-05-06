80c1881bfaffd541806df656bc9fc7d14882e54f
CypGttn <154144790+CypGttn@users.noreply.github.com>
2025-05-06 11:57:10 +0200

    Update init_with_data.js

--- init/init_with_data.js
+++ init/init_with_data.js
@@ -5,8 +5,16 @@ db.createCollection("user");
 db.createCollection("message");
 
 db.user.insertMany([
-    { username: "alice", password: "alice" },
-    { username: "bob", password: "bob" }
+    {
+        username: "alice",
+        // hash de "alice" avec bcrypt (ex: saltRounds=10)
+        password: "$2b$10$JH9U0mlYdOZUn1Yh1K27ZOD1xEY7DSo0U.mPZsvSvmxbn0IKFuj4a"
+    },
+    {
+        username: "bob",
+        // hash de "bob"
+        password: "$2b$10$C2BDRrsxey2RJuTkVWc0.eTy6U1DoDXGCLEqDTRRppn2uz4VpFEbC"
+    }
 ]);
 
 db.message.insertOne({
