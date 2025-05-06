24f769c7a7896108f9b95e91b315d05a217e60e4
Emma Rechon Reguet <emma.rechon@live.fr>
2025-05-06 09:19:39 +0200

    Vérification du mot de passe et de l'username (pas chaine d'entiers)

--- src/connexion.py
+++ src/connexion.py
@@ -1,14 +1,21 @@
 
 
+import re
+
+
 class Connexion :
     def __init__ (self, username, password, liste_user):
+        self.connecte = False
         # On récupère les infos du user choisit avec id_select
-        user = liste_user.find_one({"username": username})
-        if username == user["username"] and password == user["password"]:
-            print("Connexion réussie !")
-            print(f"Bienvenue {username} !")
-            self.connecte=True
-        else:
-            print("Identifiant ou mot de passe incorrect.")
+        if re.match(r'^[^\d]+$', username) and re.match(r'^[^\d]+$', password):
+            user = liste_user.find_one({"username": username})
+            if username == user["username"] and password == user["password"]:
+                print("Connexion réussie !")
+                print(f"Bienvenue {username} !")
+                self.connecte=True
+            else:
+                print("Identifiant ou mot de passe incorrect.")
+        else : 
+            print("Veuillez saisir deux chaînes de caractères.")
             
     
