0cb17bec5a5374aa9316709024c212e2b438226f
PULCINI-Livio <pulcinilivio04@gmail.com>
2025-05-06 10:41:38 +0200

    Update client_socket.py

--- src/client_socket.py
+++ src/client_socket.py
@@ -2,14 +2,14 @@ import socket
 import json
 
 class ClientSocket:
-    def __init__(self, host="localhost", port=9000):
+    def __init__(self, host="10.7.178.239", port=9000):  # Utilise l'IP du serveur ici
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((host, port))
+        self.sock.connect((host, port))  # Connexion au serveur via l'IP et le port
 
     def send_request(self, request: dict) -> dict:
-        self.sock.send(json.dumps(request).encode())
-        data = self.sock.recv(8192).decode()
-        return json.loads(data)
+        self.sock.send(json.dumps(request).encode())  # Envoie la requête en JSON
+        data = self.sock.recv(8192).decode()  # Réception de la réponse
+        return json.loads(data)  # Retourne la réponse sous forme de dictionnaire
 
     def close(self):
         self.sock.close()
