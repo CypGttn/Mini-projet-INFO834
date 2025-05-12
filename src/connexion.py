85bc77cd3a7faffeec2013cf3aadbcb52169473e
CypGttn <154144790+CypGttn@users.noreply.github.com>
2025-05-12 19:47:46 +0200

    Update connexion.py

--- src/connexion.py
+++ src/connexion.py
@@ -1,5 +1,6 @@
 import re
 import bcrypt
+from bson.binary import Binary
 
 class Connexion:
     def __init__(self, username, password, liste_user):
@@ -7,11 +8,20 @@ class Connexion:
 
         if re.match(r'^[^\d]+$', username) and re.match(r'^[^\d]+$', password):
             user = liste_user.find_one({"username": username})
-            if user and bcrypt.checkpw(password.encode('utf-8'), user['password']):
-                print("Connexion réussie !")
-                print(f"Bienvenue {username} !")
-                self.connecte = True
-            else:
-                print("Identifiant ou mot de passe incorrect.")
+            if user:
+                hash_mdp = user['password']
+                
+                # Si le hash est stocké comme Binary, le convertir en bytes
+                if isinstance(hash_mdp, Binary):
+                    hash_mdp = bytes(hash_mdp)
+
+                # Vérification du mot de passe
+                if bcrypt.checkpw(password.encode('utf-8'), hash_mdp):
+                    print("Connexion réussie !")
+                    print(f"Bienvenue {username} !")
+                    self.connecte = True
+                    return
+
+            print("Identifiant ou mot de passe incorrect.")
         else:
             print("Veuillez saisir deux chaînes de caractères.")
