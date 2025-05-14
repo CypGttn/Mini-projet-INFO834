d032529f4d54e2cd847c7805d78b71780c556cf1
CypGttn <154144790+CypGttn@users.noreply.github.com>
2025-05-14 09:22:29 +0200

    Update init_with_data.js

--- init/init_with_data.js
+++ init/init_with_data.js
@@ -7,11 +7,11 @@ db.createCollection("message");
 db.user.insertMany([
     {
         username: "alice",
-        password: Binary.createFromBase64("JDJiJDEwJEpIOVUwbWxZZE9aVW4xWWgxSzI3Wk9EMXhFWTdEU28wVS5tUFpzdlN2bXhibjBJS0Z1ajRh", 0)
+        password: "alice"
     },
     {
         username: "bob",
-        password: Binary.createFromBase64("JDJiJDEwJkMyQkRScnN4ZXkyUkp1VGtWV2MwLmVUeTZVMURvRFhHQ0xFcUREUlJwcG4ydXo0VnBGRWJD", 0)
+        password: "bob"
     }
 ]);
 
